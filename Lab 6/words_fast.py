30e31c986e273691b38a8c1665f8193b4e41c1d8
Ryan Stillings <rystills@gmail.com>
2017-03-24 12:30:53 -0400

    Added modified simple word ladder

--- Lab 6/words_fast.py
+++ Lab 6/words_fast.py
@@ -29,6 +29,23 @@ import networkx as nx
 #-------------------------------------------------------------------
 #   The Words/Ladder graph of Section 1.1
 #-------------------------------------------------------------------
+def generate_graph_modified(words):
+    from string import ascii_lowercase as lowercase
+    G = nx.Graph(name="words")
+    lookup = dict((c,lowercase.index(c)) for c in lowercase)
+    def edit_distance_one(word):
+        for i in range(len(word)):
+            left, c, right = word[0:i], word[i], word[i+1:]
+            j = lookup[c] # lowercase.index(c)
+            for cc in lowercase[j+1:]:
+                yield left + cc + right
+    candgen = ((word, cand) for word in sorted(words)
+               for cand in edit_distance_one(word) if cand in words)
+    G.add_nodes_from(words)
+    for word, cand in candgen:
+        G.add_edge(word, cand)
+    return G
+
 def generate_graph(words):
     from string import ascii_lowercase as lowercase
     G = nx.Graph(name="words")
